0ac3fc018ce4d194c6cd1635b3019ca26bdb3d61
Frank Aakvik <frank.aakvik@kantega.no>
2020-07-01 14:03:36 +0200

    Add shipping request transfer object.

---
+++ library/src/main/java/no/kantega/vipps/dto/ShippingRequestDTO.java
@@ -0,0 +1,16 @@
+package no.kantega.vipps.dto;
+
+public class ShippingRequestDTO {
+
+    private Integer addressId;
+    private String addressLine1;
+    private String addressLine2;
+    private String city;
+    private String country;
+    private String postCode;
+    private char addressType;
+
+    public ShippingRequestDTO() {
+
+    }
+}
