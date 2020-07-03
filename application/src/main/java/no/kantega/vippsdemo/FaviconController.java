fd07006511b96b92d8b9b2ba5a1ab34080bc9859
Frank Aakvik <frank.aakvik@kantega.no>
2020-07-03 14:22:36 +0200

    Add controller for overriding favicon.

---
+++ application/src/main/java/no/kantega/vippsdemo/FaviconController.java
@@ -0,0 +1,14 @@
+package no.kantega.vippsdemo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+class FaviconController {
+
+    @GetMapping("favicon.ico")
+    @ResponseBody
+    void returnNoFavicon() {
+    }
+}
